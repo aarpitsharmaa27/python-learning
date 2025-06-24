f28019e85cc8182472a20875c70d5529e68b9897
Arpit <arpit8894350818@gmail.com>
2025-06-24 20:13:41 +0530

    Add files via upload

---
+++ variable_practice.py.py
@@ -0,0 +1,37 @@
+
+                        #   VARIABLES
+
+# Strings (series of text)
+print("I love you")
+
+Name = "Arpit"
+Food = "Chowmin"
+Id = "aarpitsharmaa27"
+print(f"Hello Iam {Name} ")
+print(f"I love {Food}")
+print(f"My Insta Id is {Id}")
+
+# Integers (Whole Numbers)
+age = 28
+quantity = 9
+print(f"Am {age} years old")
+print(f"I will buy {quantity} items")
+
+
+# Float (Decimals)
+Price = 8.34
+print(f"Its Price is {Price}")
+
+# Boolean
+
+is_student = False
+for_sale = True
+if is_student:
+    print("You are a student")
+else:
+    print("You are not a student")
+
+if for_sale:
+    print("This item is for sale")
+else:
+    print("This item is not for sale")
