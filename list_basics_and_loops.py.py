1eca3ff587ed12f537b196526f8200b1f9feb9d8
Arpit <arpit8894350818@gmail.com>
2025-06-25 23:23:25 +0530

    Add files via upload
    Practice: Python list operations and loop usage

---
+++ list_basics_and_loops.py.py
@@ -0,0 +1,29 @@
+                         # LIST
+
+# List is collection of items i.e Complex Datatypes
+
+marks = [95, 97, 94, 82 ,77]  # for list we use square brackets
+print(marks)
+
+marks2 = [78,90,86,87]
+print(marks[0])     # For zeroth index
+
+marks3 = [78,90,86,87]
+print(marks[2])
+
+marks4 = [95, 97, 94, 82 ,77]
+print(marks4[-3])   # negative sign means count from reverse side
+
+marks5 = [89, 99, 87, 96, 84]
+print(marks5[1:3])    # If I dont want full marks ( Ending index doesnt include)
+
+
+
+# Loop use with list
+
+marks = [90, 98, 87, 88, 94]
+
+for score in marks:
+    print(score)
+
+
