a4e1cfba83698153532df0e9890228e6023aba87
Arpit <arpit8894350818@gmail.com>
2025-07-02 21:37:07 +0530

    Add files via upload

---
+++ operators.py.py
@@ -0,0 +1,58 @@
+print(5 + 2)
+print(10 * 2)
+print(15 / 3)
+print(32 - 983)
+
+
+print(20/3)
+print(20//3)  # To get answer without decimal
+
+print(10%4)   # Remainder
+
+print(6 ** 2)   # Power
+
+
+# Comparison Operators
+
+print(3 > 2)
+print(3 < 2)
+print(3 >= 2)
+print(3 <= 2)
+print(3 == 2)
+print(3 != 2)
+
+#Logical Operators  ( 3 )
+
+print(2>3 or 2 >1)  #If atleast Atleast one is true so it give output True , For False it need both input false
+print(3<1 or 10 <9)
+
+
+print(29<3 and 90 <100)
+print(10>2 and 90>23)    #If both input are true then it give True output wanna False
+
+
+print(2>3)  # with not True Become False and False become False
+print(not 2>3)
+
+i = 90
+i *= 10
+print(i)
+
+
+
+# if - else
+
+
+age = 101
+if age <= 290:
+    print("")
+elif age > 25 and age < 61:
+    print("Welcome buddy !!")
+elif age >= 61 and age <= 70:
+    print("Sorry !! .")
+elif age >= 71 and age <= 101:
+    print("OMG !! Very Senior Citizen.")
+else:
+    print("S=")
+
+
