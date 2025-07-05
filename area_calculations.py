49f00f7c5d577547fca5c4550c4dc941da4dc140
Arpit <arpit8894350818@gmail.com>
2025-07-05 22:58:57 +0530

    Add files via upload
    We can calculate area by adding different values.

---
+++ area_calculations.py
@@ -0,0 +1,18 @@
+
+    ## To Find Area of Rectangle
+
+lengh = int(input("What is the Lenght of rectangle ?:"))
+breadth = int(input("What is the breadth ?:"))
+
+Area = (lengh * breadth)
+
+print(f"The area of Rectangle is {Area}cm²")  # for ² firstly check num lock is on then hold Alt + 0178
+
+
+    ## To Find Area of Square
+
+Side = int(input("Side of Squre ? :"))
+
+Area = Side * Side
+print(f"The area of Square is {Area}cm²")
+
