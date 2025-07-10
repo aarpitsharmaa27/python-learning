b966b48ed78c12ac001354053656b747d90c60e0
Arpit <arpit8894350818@gmail.com>
2025-07-10 21:35:13 +0530

    Add files via upload
    Different string methods in Python.

---
+++ string_methods.py
@@ -0,0 +1,16 @@
+# name = input("Enter your full name :")
+phone_number = input("Enter your phone number :")
+
+# result = len(name)
+# result = name.find("T") --- get value of any alphabet of your input
+# result = name.rfind("r") --- get value of any alphabet of your input
+# result = name.capitalize() --- to get first letter of output in upper case
+# result = name.upper() --- to see input in upper characters
+# result = name.lower() --- to see input in lower characters
+# result = name.isdigit() --- shows Boolen value for input i.e if its only have digits it show true else false
+# result = name.isalpha() --- shows Boolen value for input i.e if its only have alphabets(without space) it show true else false
+
+# result = phone_number.count("-") --- it count dashes in input phone number
+# result = phone_number.replace("-", "")  --- replaces sign
+
+print(result)
