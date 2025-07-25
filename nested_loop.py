13ce5a0cf470987c04066ddd18d81184014eb740
Arpit <aarpitsharmaa27@gmail.com>
2025-07-25 21:16:05 +0530

    Add files via upload
    Nested loop with examples.

---
+++ nested_loop.py
@@ -0,0 +1,22 @@
+# nested loop = A loop within another loop (outer , inner)
+#               outer loop :
+#                       inner loop:
+
+
+#for x in range (1 , 11):
+#    print(x)                    # WIth this output prints vertically
+
+#for x in range (1 , 11):
+ #   print(x, end=" ")           # WIth this output prints horizontallly with space
+
+# ----------------------- EXAMPLE -------------------------------------------------------------------------
+
+for x in range(3):                # it executes code three times
+    for x in range (1 , 11):
+            print(x, end="")           # WIth this output prints horizontallly
+    print()                      # it prints output in three different lines
+
+
+
+
+
